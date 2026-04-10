7be3fe5885a39c795cbd69bea140e8582e9ff9d4
Naman Pandit <Namanpandit2508@gmail.com>
2026-04-10 12:38:10 +0530

    updated for google login

--- src/api/auth.js
+++ src/api/auth.js
@@ -7,7 +7,14 @@ const BASE_URL =
 /* ================= AXIOS INSTANCE ================= */
 const API = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // 
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 /* ================= AUTH ================= */
